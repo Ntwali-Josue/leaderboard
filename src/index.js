1cc5338953ff636ede26a868f042f90f510479d8
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-02 17:52:52 +0200

    Add fetch scores function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import './style.css';
+import fetchOnRefresh from './refreshScores';
+import addScores from './addScores';
 
 const root = document.querySelector('.root');
 
-const fetchOnRefresh = async () => {
+const fetchScores = async () => {
   let results;
   await fetch(
     'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Dgl1g5JXUz6joOBlRdMy/scores',
   ).then((response) => response.json())
     .then((json) => {
       const ul = document.querySelector('#scores');
-      ul.innerHTML = '';
       for (let i = 0; i < json.result.length; i += 1) {
         results = json.result[i];
         const list = document.createElement('li');
@@ -34,18 +35,17 @@ const leaderboard = () => {
     <button type="button" class="refresh button btn mx-5">Refresh</button>
   </div>
   <div class="player-scores mt-4">
-    <span class="px-2">Click the Refresh button to load the scores.<span>
     <ul class="list-group" id="scores"></ul>
   </div>
 </div>
 <div class="container">
   <form>
     <h3 class="mb-4">Add your score</h3>
-    <input type="text" placeholder="Your name" required /> <br />
+    <input type="text" placeholder="Your name" class="names" required /> <br />
     <br />
-    <input type="text" placeholder="Your score" required /> <br />
+    <input type="number" placeholder="Your score" class="scores" required /> <br />
     <br />
-    <input type="submit" value="Submit" class="button btn" />
+    <input type="submit" value="Submit" class="button submit btn" />
   </form>
 </div>`;
 
@@ -54,6 +54,10 @@ const leaderboard = () => {
 
 leaderboardTitle();
 leaderboard();
+fetchScores();
 
 const refreshButton = document.querySelector('.refresh');
 refreshButton.addEventListener('click', fetchOnRefresh);
+
+const submitScores = document.querySelector('.submit');
+submitScores.addEventListener('click', addScores);
